b27f0616c9732a376a0d643ab2ff4aa265e2c861
Mashhood Jeelani <mashhood,jeelani@gmail.com>
2020-07-17 15:33:33 +0530

    Dice Roll Game Unfinished

---
+++ index.js
@@ -0,0 +1,39 @@
+var rand1 = Math.floor(Math.random()*6+1);
+var rand2 = Math.floor(Math.random()*6+1);
+
+if(rand1>rand2)
+  document.querySelector("h1").textContent = "🚩 Player 1 Wins";
+else if(rand1<rand2)
+  document.querySelector("h1").textContent = "Player 2 Wins 🚩";
+else
+  document.querySelector("h1").textContent = "Draw! Refresh Again.";
+
+
+
+if (rand1===1){
+  document.querySelector(".img1").setAttribute("src","images/dice1.png");}
+  if (rand1===2){
+    document.querySelector(".img1").setAttribute("src","images/dice2.png");}
+    if (rand1===3){
+      document.querySelector(".img1").setAttribute("src","images/dice3.png");}
+      if (rand1===4){
+        document.querySelector(".img1").setAttribute("src","images/dice4.png");}
+        if (rand1===5){
+          document.querySelector(".img1").setAttribute("src","images/dice5.png");}
+          if (rand1===6){
+            document.querySelector(".img1").setAttribute("src","images/dice6.png");}
+
+
+
+            if (rand2===1){
+              document.querySelector(".img2").setAttribute("src","images/dice1.png");}
+              if (rand2===2){
+                document.querySelector(".img2").setAttribute("src","images/dice2.png");}
+                if (rand2===3){
+                  document.querySelector(".img2").setAttribute("src","images/dice3.png");}
+                  if (rand2===4){
+                    document.querySelector(".img2").setAttribute("src","images/dice4.png");}
+                    if (rand2===5){
+                      document.querySelector(".img2").setAttribute("src","images/dice5.png");}
+                      if (rand2===6){
+                        document.querySelector(".img2").setAttribute("src","images/dice6.png");}
